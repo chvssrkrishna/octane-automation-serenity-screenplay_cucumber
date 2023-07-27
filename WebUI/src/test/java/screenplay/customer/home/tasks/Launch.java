53de06991628bd7944610711b1a34e5e80035fa1
chvssrkrishna <37030728+chvssrkrishna@users.noreply.github.com>
2023-07-28 00:43:51 +0530

    serenity configurations updated

--- WebUI/src/test/java/screenplay/customer/home/tasks/Launch.java
+++ WebUI/src/test/java/screenplay/customer/home/tasks/Launch.java
@@ -14,29 +14,12 @@ import java.time.Duration;
 
 public class Launch {
 
-    private static Task clickIconButton(String button) {
-        return Task.where(
-                "{0} open Chat Window by Clicking on icon button " + button,
-                Click.on(HomePage.ICON_BUTTON.of(button)),
-                WaitForElement.visibility(HomePage.CHATPANEL_IFRAME, WaitConstants.getExplicitWait()),
-                Switch.toFrame(HomePage.CHATPANEL_IFRAME_NAME)
-        );
-
-    }
-
-    private static Task selectDepartment(String department) {
-        return Task.where(
-                "{0} selected department : " + department,
-                Check.whether(Presence.of(ChatPanel.DEPARTMENT_SELECTOR.of(department).waitingForNoMoreThan(Duration.ofSeconds(WaitConstants.getExplicitWait())))).andIfSo(Click.on(ChatPanel.DEPARTMENT_SELECTOR.of(department))),
-                WaitForElement.visibility(ChatPanel.CHATPANEL_ENTER_YOUR_MESSAGE, WaitConstants.getExplicitWait())
-        );
-
-    }
-
     public static Task chat_window_via_IconButton(String button) {
         return Task.where(
                 "{0} open Chat Window via Icon Button " + button,
-                Launch.clickIconButton(button)
+                Click.on(HomePage.ICON_BUTTON.of(button)),
+                WaitForElement.visibility(HomePage.CHATPANEL_IFRAME, WaitConstants.getExplicitWait()),
+                Switch.toFrame(HomePage.CHATPANEL_IFRAME_NAME)
         );
 
     }
@@ -45,7 +28,7 @@ public class Launch {
 
         return Task.where(
                 "{0} open Chat Window via Icon Button " + button,
-                Launch.clickIconButton(button),
+                Launch.chat_window_via_IconButton(button),
                 Check.whether(Presence.of(ChatPanel.DEPARTMENT_SELECTOR_FORM.waitingForNoMoreThan(Duration.ofSeconds(WaitConstants.getExplicitWait())))).andIfSo(Click.on(ChatPanel.DEFAULT_DEPARTMENT_SELECTOR)),
                 WaitForElement.visibility(ChatPanel.CHATPANEL_ENTER_YOUR_MESSAGE, WaitConstants.getExplicitWait())
         );
@@ -55,8 +38,9 @@ public class Launch {
     public static Task chat_window_IconButton_DepartmentSelector(String button,String department) {
         return Task.where(
                 "{0} open Chat Window via Icon Button " + button+" and department "+department,
-                Launch.clickIconButton(button),
-                Launch.selectDepartment(department)
+                Launch.chat_window_via_IconButton(button),
+                Check.whether(Presence.of(ChatPanel.DEPARTMENT_SELECTOR.of(department).waitingForNoMoreThan(Duration.ofSeconds(WaitConstants.getExplicitWait())))).andIfSo(Click.on(ChatPanel.DEPARTMENT_SELECTOR.of(department))),
+                WaitForElement.visibility(ChatPanel.CHATPANEL_ENTER_YOUR_MESSAGE, WaitConstants.getExplicitWait())
         );
 
     }
--- WebUI/src/test/resources/messagenow/launch.feature
+++ WebUI/src/test/resources/messagenow/launch.feature
@@ -1,10 +1,21 @@
+@launch
 Feature:launch feature
-
-
+#mvn clean verify -Dfailsafe.rerunFailingTestsCount=1 -Dtags="launch"
+# mvn clean verify -Dfailsafe.rerunFailingTestsCount=1 -Dtags="launch"  -Dserenity.outputDirectory=target/site/messagenow -Dserenity.project.name=messagenow
+  @launch1
   Scenario Outline: launch customer home page
     Given krishna launch customer home page of dealer "<dealer>"
     When krishna open chat panel from "Chat" Icon Button
     Then krishna send "hello agent" message to dealer "<dealer>"
+    Then krishna submit contact information
+
+    Examples:
+      | dealer         |
+      | krishna_toyota |
+    @launch2
+  Scenario Outline: launch customer home page Testing
+    Given krishna launch customer home page of dealer "<dealer>"
+    When krishna open chat panel from "Chat" Icon Button
     Then krishna submit contact information
 
     Examples:
